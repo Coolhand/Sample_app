c514e186f776681f0a1a2bc6955ad6d49ad05bf3
Nick <niharvey@redhat.com>
2012-10-24 09:36:49 -0400

    switching

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User
   include Mongoid::Document
   field :name, type: String
   field :email, type: String
+  index({ email: 1 }, { unique: true })
+  
+  before_save { |user| user.email = email.downcase }
   
   validates(:name, presence: true, length: { maximum: 50 })
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }  
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
